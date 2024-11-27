8606000d63abeb8e6cfcfa694d472534276a1b8c
Vaibhav2902 <vaibhav.singh@qyuki.com>
2024-11-27 16:26:58 +0530

    middleware

--- nextjs-blog-app/src/middleware.js
+++ nextjs-blog-app/src/middleware.js
@@ -7,5 +7,4 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)', // Always run for API routes
   ],
-  runtime: 'nodejs', // Ensure the runtime is Node.js
 };
